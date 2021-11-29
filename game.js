a1d476056cc9cc706f24ebf09788d5ff922a3ba3
ControlledChaos0 <jade.spoon2003@gmail.com>
2021-11-29 18:56:29 -0500

    Yoooooooooooo

--- game.js
+++ game.js
@@ -1,87 +1,80 @@
 kaboom({
     global:true,
-    width: 500,
-    height: 500,
+    fullscreen: false,
     scale: 1,
+    width: 1000,
+    height: 600,
     debug: true,
     clearColor: [0, 0, 0, 1],
-})
+});
 
 // Speed identifiers
-const MOVE_SPEED = 180
-const JUMP_FORCE = 360
-const BIG_JUMP_FORCE = 550
-let CURRENT_JUMP_FORCE = JUMP_FORCE
-const FALL_DEATH = 400
-const ENEMY_SPEED = 20
+const MOVE_SPEED = 200;
+const JUMP_FORCE = 800;
+const BIG_JUMP_FORCE = 2000;
+let CURRENT_JUMP_FORCE = JUMP_FORCE;
+const FALL_DEATH = 400;
+const ENEMY_SPEED = 20;
 
 //Game logic
-let isJumping = true
-loadSprite('ground', 'sprites/ground-dead.png')
-loadSprite('bot', 'sprites/bot2.png')
-
+let isJumping = true;
+loadRoot('https://i.imgur.com/');
+loadSprite('ground', 'sprites/ground-dead.png');
+loadSprite('bot', 'sprites/bot2.png');
+loadSprite('enemy', 'xqkDjqT.png');
 
 
 scene("game", ({ level, score }) => {
-  layers(['bg', 'obj', 'ui'], 'obj')
+  layers(['bg', 'obj', 'ui'], 'obj');
 
   const maps = [
     [
-      '                                                                                ',
-      '                                                                                ',
-      '                                                                                ',
-      '                                                                                ',
-      '                                                                                ',
-      '                                                                                ',
-      '                                                                                ',
-      '                                                                                ',
-      '                                                                                ',
-      '================================================================================',
+      '                                      ',
+      '                                      ',
+      '                                      ',
+      '======================================',
     ],
-  ]
+  ];
+
+  const player = add([
+    sprite('bot'), solid(),
+    pos(200, 392),
+    body(),
+    origin('bot')
+  ]);
 
   const levelCfg = {
     width: 64,
     height: 64,
+    pos: vec2(0, 200),
     '=': [sprite('ground'), solid()],
-  }
+  };
 
-  const gameLevel = addLevel(maps[level], levelCfg)
-
-  const player = add([
-    sprite('bot'), solid(),
-    pos(30, 0),
-    body(),
-    origin('bot')
-  ])
+  const gameLevel = addLevel(maps[level], levelCfg);
 
   player.action(() => {
-    camPos(player.pos)
     if (player.pos.y >= 1000) {
-      go('lose')
+      go('lose');
     }
-  })
+  });
 
-  keyDown('left', () => {
-    player.move(-MOVE_SPEED, 0)
-  })
-
-  keyDown('right', () => {
-    player.move(MOVE_SPEED, 0)
-  })
+  gravity(2700);
 
   player.action(() => {
     if(player.grounded()) {
-      isJumping = false
+      isJumping = false;
     }
-  })
+  });
 
-  keyPress('space', () => {
+  keyDown('space', () => {
     if (player.grounded()) {
-      isJumping = true
-      player.jump(CURRENT_JUMP_FORCE)
+      player.jump(CURRENT_JUMP_FORCE);
     }
-  })
+  });
+
+  /**
+   * Enemy control
+   */
 
   /**
   * Adding controls for audio 
@@ -89,15 +82,15 @@ scene("game", ({ level, score }) => {
   let music = document.getElementById("music");
   document.getElementById("music").loop = true;
   function playAudio() {
-    music.play();
+    //music.play();
   }
   keyPress('right', () => {
     playAudio();
   })
-})
+});
 
 scene('lose', () => {
   add([text("You Lose...")])
-})
+});
 
-start("game", { level: 0})
+start("game", { level: 0});
